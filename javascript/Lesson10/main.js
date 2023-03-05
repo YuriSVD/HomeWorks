6221adb59a1b330fc078239c53cbc2c8e824eddc
YuriSVD <yu.svyda@gmail.com>
2023-03-05 20:28:56 +0200

    Add Lesson 10

--- javascript/Lesson10/main.js
+++ javascript/Lesson10/main.js
@@ -35,8 +35,8 @@ onload = function () {
 // ==========================
 // Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessions зберігається інформація про дату та час відвідування сторінки. Є ще сторінка sessions.html (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль, а побудувати дом структуру під кожну сессію
 onunload = function () {
-    let sessionsArray =JSON.parse(localStorage.getItem("sessions")) || [];
-    sessionsArray.push(new Date());
+    let sessionsArray = JSON.parse(localStorage.getItem("sessions")) || [];
+    sessionsArray.push(new Date().toLocaleString());
     localStorage.setItem("sessions", JSON.stringify(sessionsArray));
 }; // розумію що трохи говнокоду, але так і не зрозумів до якого елементу використати addEventListener що використати onload або onunload
 // =========================
